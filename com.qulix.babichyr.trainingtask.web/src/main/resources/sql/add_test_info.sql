4fa2682366877eb36791898f4e331108a1c6a457
turquoise-sun <jjouyr@gmail.com>
2015-07-20 00:13:05 +0300

    Added sql scripts, updated database files

---
+++ com.qulix.babichyr.trainingtask.web/src/main/resources/sql/add_test_info.sql
@@ -0,0 +1,3 @@
+INSERT INTO EMPLOYEE values (null, 'Yaraslau', 'Babich', 'Ruslanavich', 'Programmer');
+INSERT INTO PROJECT values (null, 'Application Dev', 'AD', 'The dev process');
+INSERT INTO TASK values (null, 0,0, 'Info Gather', 10, '2015-07-09', '2015-07-13', 'Not Started');
